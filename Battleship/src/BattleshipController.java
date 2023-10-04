32fc2d539d1bd32d9b29ac2036680964c471df51
patelmeet1542 <144351356+patelmeet1542@users.noreply.github.com>
2023-10-04 11:55:57 -0400

    Update BattleshipController.java

--- Battleship/src/BattleshipController.java
+++ Battleship/src/BattleshipController.java
@@ -1,4 +1,13 @@
+import javax.swing.JFrame;
 
 public class BattleshipController {
 
+	public static void main(String[] args) {
+
+    BattleshipModel gameModel = new BattleshipModel();
+		BattleshipView gameView = new BattleshipView(gameModel);
+		
+		
+	}
+	
 }
